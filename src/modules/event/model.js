2d4621f47942bb4d4f153b1d522cc395af4582e8
SerenityScript <yuliasiebrandt89@gmail.com>
2026-01-14 09:05:44 +0100

    event model updated

--- src/modules/event/model.js
+++ src/modules/event/model.js
@@ -1,19 +1,24 @@
 const mongoose = require('mongoose');
 
-const eventSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  date: {
-    type: String,
-    required: true,
-    match: /^\d{4}-\d{2}-\d{2}$/
+const eventSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+
+    date: {
+      type: String,
+      required: true,
+      match: /^\d{4}-\d{2}-\d{2}$/
+    },
+
+    time: {
+      type: String,
+      required: true,
+      match: /^([01]\d|2[0-3]):[0-5]\d$/
+    },
+
+    location: { type: String, required: true },
   },
-  // "15:00"
-  time: {
-    type: String,
-    required: true,
-    match: /^([01]\d|2[0-3]):[0-5]\d$/
-  },
-  location: { type: String, required: true }
-})
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Event', eventSchema);
