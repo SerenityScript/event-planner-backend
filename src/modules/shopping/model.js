8b2ca93419c2a3afb300e4d245b5c2524e789f76
SerenityScript <yuliasiebrandt89@gmail.com>
2026-01-14 09:06:52 +0100

    shopping model updated

--- src/modules/shopping/model.js
+++ src/modules/shopping/model.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
 
-const shoppingItemSchema = new mongoose.Schema({
-  eventId: { type: mongoose.Schema.Types.ObjectId, ref: 'Event', required: true },
-  name: { type: String, required: true },
-  category: { 
-    type: String,
-    enum: ['food', 'drinks', 'decor', 'other'],
-    default: 'food',
-    required: true
+const shoppingItemSchema = new mongoose.Schema(
+  {
+    eventId: { type: mongoose.Schema.Types.ObjectId, ref: 'Event', required: true },
+    name: { type: String, required: true },
+    category: { 
+      type: String,
+      enum: ['food', 'drinks', 'decor', 'other'],
+      default: 'food',
+      required: true
+    },
+    qty: { type: String, required: false },
+    bought: { type: Boolean, default: false }
   },
-  qty: { type: String, required: false },
-  bought: { type: Boolean, default: false }
-})
+  { timestamps: true }
+)
 
 module.exports = mongoose.model('ShoppingItem', shoppingItemSchema);
