a01c596b1ed9759906cac502356d57377aea7e13
SerenityScript <yuliasiebrandt89@gmail.com>
2026-01-14 09:06:22 +0100

    guest model updated

--- src/modules/guest/model.js
+++ src/modules/guest/model.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose');
 
-const guestSchema = new mongoose.Schema({
-  eventId: { type: mongoose.Schema.Types.ObjectId, ref: 'Event', required: true },
-  name: { type: String, required: true },
-  status: { 
-    type: String,
-    enum: ['invited', 'confirmed', 'declined'],
-    default: 'invited',
-    required: true
-  }
-})
+const guestSchema = new mongoose.Schema(
+  {
+    eventId: { type: mongoose.Schema.Types.ObjectId, ref: 'Event', required: true },
+    name: { type: String, required: true },
+    status: { 
+      type: String,
+      enum: ['invited', 'confirmed', 'declined'],
+      default: 'invited',
+      required: true
+    }
+  },
+  { timestamps: true }
+)
 
 module.exports = mongoose.model('Guest', guestSchema);
