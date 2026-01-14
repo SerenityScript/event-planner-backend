b7dab102f0f9553e4783d30899cd09584887f77c
SerenityScript <yuliasiebrandt89@gmail.com>
2026-01-14 09:07:25 +0100

    task model updated

--- src/modules/task/model.js
+++ src/modules/task/model.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 
-const taskSchema = new mongoose.Schema({
-  eventId: { type: mongoose.Schema.Types.ObjectId, ref: 'Event', required: true },
-  title: { type: String, required: true },
-  done: { type: Boolean, default: false }
-})
+const taskSchema = new mongoose.Schema(
+  {
+    eventId: { type: mongoose.Schema.Types.ObjectId, ref: 'Event', required: true },
+    title: { type: String, required: true },
+    done: { type: Boolean, default: false }
+  },
+  { timestamps: true }
+)
 
 module.exports = mongoose.model('Task', taskSchema);
